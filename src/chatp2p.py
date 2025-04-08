61c4b4044465e42eee365f6f01d06ec83afd9268
murillodmf <murillodmferraz@gmail.com>
2025-04-08 09:41:04 -0300

    codigo finalizado

--- src/chatp2p.py
+++ src/chatp2p.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+
 # Thread para escutar mensagens recebidas
 def receive_messages(sock):
     while True:
@@ -15,6 +16,7 @@ def receive_messages(sock):
             print(f"[ERRO AO RECEBER] {e}")
             break
 
+
 # Thread para enviar mensagens
 def send_messages(sock):
     while True:
@@ -24,6 +26,7 @@ def send_messages(sock):
         except Exception as e:
             print(f"[ERRO AO ENVIAR] {e}")
             break
+
 
 # Thread que escuta conexões (modo servidor)
 def server_thread(local_ip, local_port):
@@ -38,6 +41,7 @@ def server_thread(local_ip, local_port):
     threading.Thread(target=receive_messages, args=(conn,), daemon=True).start()
     threading.Thread(target=send_messages, args=(conn,), daemon=True).start()
 
+
 # Função para conectar ao peer (modo cliente)
 def connect_to_peer(dest_ip, dest_port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -46,26 +50,30 @@ def connect_to_peer(dest_ip, dest_port):
         print(f"[CONECTADO COMO CLIENTE] a {dest_ip}:{dest_port}")
 
         # Cria threads de envio e recebimento
-        threading.Thread(target=receive_messages, args=(client,), daemon=True).start()
-        threading.Thread(target=send_messages, args=(client,), daemon=True).start()
+        receivethread = threading.Thread(target=receive_messages, args=(client,), daemon=True)
+        receivethread.start()
+
+        sendthread = threading.Thread(target=send_messages, args=(client,), daemon=True)
+        sendthread.start()
+
+        receivethread.join()
+        sendthread.join()
 
     except Exception as e:
         print(f"[FALHA NA CONEXÃO] {e}")
 
+
 # Programa principal
 if __name__ == "__main__":
     print("=== Chat TCP Peer to Peer ===")
-    local_ip = input("Digite seu IP local: ")
-    local_port = int(input("Digite sua porta local: "))
-    dest_ip = input("Digite o IP do peer de destino: ")
-    dest_port = int(input("Digite a porta do peer de destino: "))
-
-    # Inicia o servidor numa thread separada
-    threading.Thread(target=server_thread, args=(local_ip, local_port), daemon=True).start()
-
-    # Tenta se conectar ao outro peer
-    connect_to_peer(dest_ip, dest_port)
-
-    # Mantém o programa rodando
-    while True:
-        pass
+    escolha = input("Digite se voce deseja se conectar ou ser conectado 'c' ou 'l' \n")
+    if escolha == "l":
+        local_ip = input("Digite seu IP local: ")
+        local_port = int(input("Digite sua porta local: "))
+        server_thread(local_ip, local_port)
+    elif escolha == "c":
+        dest_ip = input("Digite o IP do peer de destino: ")
+        dest_port = int(input("Digite a porta do peer de destino: "))
+        connect_to_peer(dest_ip, dest_port)
+    else:
+        print("Digite'c'ou'l'")
